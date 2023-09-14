0a0d3d8eaf441937cfdffcba066f1cea3d8e669b
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-14 21:16:38 +0600

    fake data added in json formet

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold py-14">Course Registration</h1>
+        <h1 className="text-3xl font-bold pt-14 pb-10">Course Registration</h1>
+        <div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </>
   );
