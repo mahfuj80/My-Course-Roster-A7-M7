c934e1c01f31227a0862fad4c2690c9d3f11c9d0
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-14 20:34:46 +0600

    font family Inter added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="text-3xl font-bold underline">My Course Roster</h1>
+        <h1 className="text-3xl font-bold py-14">Course Registration</h1>
       </div>
     </>
   );
